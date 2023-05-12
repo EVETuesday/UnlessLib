43fb2ec5e2671fc34d188671326ca8b2df7a07c0
Максим Кириллин <Kirillin980@gmail.com>
2023-05-12 16:08:21 +0300

    Signed-off-by: Максим Кириллин <Kirillin980@gmail.com>

---
+++ TestForLib/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnlessMath;
+
+namespace TestForLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+            int c = 9;
+            double[] arr = new double[5] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(Algebra.SumArray(arr));
+            Console.WriteLine(Algebra.MaxFromArray(arr));
+            Console.WriteLine(Algebra.MinFromArray(arr));
+            Console.WriteLine(Algebra.AVGArray(arr));
+
+            Console.WriteLine(Geometry.TrianglAarea(a, b));
+            Console.WriteLine(Geometry.TrianglAarea(a, b, c));
+            Console.WriteLine(Geometry.SquareArea(a));
+            Console.WriteLine(Geometry.RectangleArea(a, b));
+            Console.WriteLine(Geometry.RhombusArea(a, b));
+            Console.WriteLine(Geometry.CircleArea(a));
+
+            Console.WriteLine(Trigonometry.Sin(c));
+            Console.WriteLine(Trigonometry.Cos(c));
+            Console.WriteLine(Trigonometry.Tgn(c));
+            Console.WriteLine(Trigonometry.Ctg(c));
+
+            Console.ReadKey();
+        }
+    }
+}
